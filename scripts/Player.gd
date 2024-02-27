eebe63c08b524499949ef1fd41a298a57e656b53
Filip Mårtensson <fenwikk@protonmail.com>
2024-02-27 17:50:50 +0100

    Go behind walls!

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,13 +6,15 @@ const SPEED = 75
 
 @export var animation: AnimationPlayer
 @export var top_tile_collider: CollisionShape2D
+@export var top_detector_collider: CollisionShape2D
 @export var bottom_tile_collider: CollisionShape2D
+@export var bottom_detector_collider: CollisionShape2D
 
 @onready var sprite = $Sprite2D
 @onready var main = get_node("/root/Main/Beans")
 
-var tileOnTop = false
-var tileOnBottom = false
+var tile_on_top = false
+var tile_on_bottom = false
 
 func _process(_delta):
 	if Input.is_action_just_pressed("fire"):
@@ -39,24 +41,28 @@ func _physics_process(_delta):
 
 	move_and_slide()
 
-func _on_bottom_tile_check_body_entered(body: Node2D):
-	if body is TileMap&&!tileOnTop:
-		tileOnBottom = true
+func _on_bottom_tile_detector_body_entered(body: Node2D):
+	if body is TileMap&&!tile_on_top:
+		tile_on_bottom = true
 		bottom_tile_collider.set_deferred("disabled", true)
+		top_detector_collider.set_deferred("disabled", true)
 		z_index = 3
 
-func _on_bottom_tile_check_body_exited(body: Node2D):
+func _on_bottom_tile_detector_body_exited(body: Node2D):
 	if body is TileMap:
-		tileOnBottom = false
+		tile_on_bottom = false
 		bottom_tile_collider.set_deferred("disabled", false)
+		top_detector_collider.set_deferred("disabled", false)
 		z_index = 5
 
-func _on_top_tile_check_body_entered(body: Node2D):
-	if body is TileMap&&!tileOnBottom:
-		tileOnTop = true
+func _on_top_tile_detector_body_entered(body: Node2D):
+	if body is TileMap&&!tile_on_bottom:
+		tile_on_top = true
 		top_tile_collider.set_deferred("disabled", true)
+		bottom_detector_collider.set_deferred("disabled", true)
 
-func _on_top_tile_check_body_exited(body: Node2D):
+func _on_top_tile_detector_body_exited(body: Node2D):
 	if body is TileMap:
-		tileOnTop = false
+		tile_on_top = false
 		top_tile_collider.set_deferred("disabled", false)
+		bottom_detector_collider.set_deferred("disabled", false)
